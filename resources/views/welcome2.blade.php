3a6607048b24831359dfed910452b55637793b1b
Vova <tomaslipton@gmail.com>
2024-08-03 21:44:43 +0200

    ui

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -46,7 +46,7 @@
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
     <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div class=" min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <!--
               Modal panel, show/hide based on modal state.
 
@@ -59,18 +59,6 @@
             -->
             <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
 
-{{--                <div class="bg-white px-4 pb-4 pt-5 sm:p-3 sm:pb-1">--}}
-{{--                    <div class="sm:flex sm:items-start">--}}
-{{--                        <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">--}}
-{{--                            <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">--}}
-{{--                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />--}}
-{{--                            </svg>--}}
-{{--                        </div>--}}
-{{--                        <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">--}}
-{{--                            <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
                 <div class="mt-2_">
                     <img id="modal-img" />
                 </div>
